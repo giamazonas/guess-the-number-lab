90ebab79075386b1d4cb7fd4b0b3bbc9cb44624b
giamazonas <skibinskirenee@gmail.com>
2022-01-30 17:20:45 -0500

    guess complete

--- app.js
+++ app.js
@@ -6,23 +6,37 @@ const game = {
   smallestNum: 1, 
   prevGuesses: [],
   secretNum: null,
+
+  getGuess: function() {
+    let guess; 
+    do {
+      guess = parseInt (prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
+    )}
+    while
+    (isNaN(guess) || guess > this.biggestNum || guess < this.smallestNum)
+    return guess
+  },
+
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-    { this.getGuess = '';
-      let i = 0;
-      do{
-      i = i + i;
-      this.prevGuesses = this.getGuess + i;
-    }while (i !== this.secretNum);
-  } 
-  // console.log(game.getGuess())
-  },
-  getGuess: function(){
-    let int = prompt()
-      return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`
-    while (int < this.biggestNum && int > this.smallestNum){
-      return input.parseInt()
-    } 
-    } 
+
+    let prevGuesses = []
+    while this.prevGuesses.push(this.getGuess() 
+    {
+      this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
+    }
+    
   }
+
+// render: function(){
+// // play calls this after guess has been made(alerts)
+// // if secret guessed, congrats message
+// // else ...
+// //use interpolation
+// /// array.join method
+// if 
+// }
+
+}
+// console.log(game.play())
