99ce8814946a1e20af15093e435ef96a98c69c5e
giamazonas <skibinskirenee@gmail.com>
2022-01-30 18:38:10 -0500

    play add

--- app.js
+++ app.js
@@ -20,23 +20,36 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-
-    let prevGuesses = []
-    while this.prevGuesses.push(this.getGuess() 
-    {
-      this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
-    }
+      
+      do {
+        this.prevGuesses.push(this.getGuess() 
+        )
+      }
+      while ( 
+        // this.getGuess !==
+        this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
+      },
     
-  }
+  render: function(){
+    let joined = preGuesses.join()
 
-// render: function(){
-// // play calls this after guess has been made(alerts)
-// // if secret guessed, congrats message
-// // else ...
-// //use interpolation
-// /// array.join method
-// if 
-// }
+    if (this.secretNum === getGuess) {
+    `Congrats! You guessed the number in ${prevGuesses.length}!`}  
 
-}
-// console.log(game.play())
+    } else (this.secretNum !== getGuess) {
+    `Your guess is too ${spectrum} Previous guesses: ${joined}.`}
+    
+  spectrum = function() 
+      if (this.secretNum < getGuess){
+        return "high"
+      }else (this.secretNum > getGuess){
+        return "low"
+      }
+    }
+
+
+// play calls this after guess has been made(alerts)
+// if secret guessed, congrats message
+// else ...
+//use interpolation
+/// array.join method
