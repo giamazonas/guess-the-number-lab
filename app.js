d13486eea4eca646ff6a290271215cdec7285787
giamazonas <skibinskirenee@gmail.com>
2022-01-30 14:03:21 -0500

    function progress

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ const game = {
   secretNum: null,
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+    { this.getGuess = '';
+      let i = 0;
+      do{
+      i = i + i;
+      this.prevGuesses = this.getGuess + i;
+    }while (i !== this.secretNum);
+  } 
+  // console.log(game.getGuess())
+  },
+  getGuess: function(){
+    let int = prompt()
+      return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`
+    while (int < this.biggestNum && int > this.smallestNum){
+      return input.parseInt()
+    } 
+    } 
   }
-}
-
-function getGuess(){
-  const parsed = parseInt();
-  if (isNaN(parsed)){
-    return parsed 
-  }return `Enter a guess between ${smallestNum} and ${biggestNum}.`
-}
-
-console.log()
