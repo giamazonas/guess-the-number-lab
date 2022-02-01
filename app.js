154407ae7442c6a23f70f9db68c584e3a609683b
giamazonas <skibinskirenee@gmail.com>
2022-01-31 20:45:59 -0500

    include render

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const game = {
       do {
         this.prevGuesses.push(this.getGuess() ) 
         this.render()
-        console.log(this.prevGuesses)
-        // prompt (`Try again! Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
+        // play function activates, so render needed after current get guess is entered into prev guesss array
+        // console.log(this.prevGuesses)
+        
       }while ( 
         this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
       },
@@ -23,22 +24,25 @@ const game = {
   getGuess: function() {
     let guess; 
     do {
-      guess = parseInt (prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
+      guess =  parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
     }while
     (isNaN(guess) || guess > this.biggestNum || guess < this.smallestNum)
     return guess 
   },
 
   render: function() {
+    let high = this.prevGuesses[this.prevGuesses.length -1] > this.biggestNum;
     if(this.secretNum === this.prevGuesses[this.prevGuesses.length -1] ){
     alert(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
-    }else{ 
-    alert(`Your guess is too ${this.high||this.low}. 
-    Previous guesses: ${this.prevGuesses.join}.`)
-  }
-//     let high = this.guess > this.biggestNum;
-//     let low = this.guess < this.smallestNum;
-}
+    }else if(
+      this.prevGuesses[this.prevGuesses.length -1] > this.biggestNum )(
+      alert(`Your guess is too high. 
+      Previous guesses: ${this.prevGuesses.join(', ')}.`)
+    )
+    else (
+      alert(`Your guess is too low. Previous guesses: ${this.prevGuesses.join(', ')}.`)
+      )
+    }
 }
 
 // game.play()
